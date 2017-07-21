2f2434640afb274857eff4ee9ff48d684c2419c4
Santiago Miguelez <santiago_miguelez@outlook.com>
2017-07-21 09:44:52 -0300

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,57 +1,58 @@
 class ArticlesController < ApplicationController
 
-  def index
-    @articles = Article.all
-  end
+before_action :set_article, only: [:edit, :destroy, :update, :show]
 
-  def new
-    @article = Article.new
-  end
+def index
+  @articles = Article.all
+end
 
-  def edit
-    @article = Article.find(params[:id])    
-  end
+def new
+  @article = Article.new
+end
 
-  def create
+def edit
+end
+
+def create
 	# for instance, if you just want to display the parameters received for the article creation:
 	# render plain: params[:article].inspect
 
-      @article = Article.new(article_params)
+  @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+end
 
-      if @article.save
-         flash[:notice] = "Article was successfully created"
-         redirect_to article_path(@article)
-      else
-         render 'new'
-      end
-   end
-
-  def update
-    @article = Article.find(params[:id])
-
+def update
     if @article.update(article_params)
       flash[:notice] = "Article was successfully modified"
       redirect_to article_path(@article)
     else
       render 'new'
     end
-  end
+end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+def show
+end
 
-  def destroy
-    @article = Article.find(params[:id])
+def destroy
     @article.destroy
     flash[:notice] = "Article wass successfully deleted"
     redirect_to articles_path
-  end
+end
 
 	
   private
-      def article_params
-         params.require(:article).permit(:title, :description)
-      end
 
+  def set_article
+    @article = Article.find(params[:id])
   end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+end
